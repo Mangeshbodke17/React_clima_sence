543fe1d56e9cd9c5e67d51977fab4dadddfe9f23
Mangeshbodke17 <mangeshbodke2025@gmail.com>
2025-06-07 00:44:54 +0530

    Set base URL for GitHub Pages and add 404 redirect

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Clima-Sence/',
+  base: '/React_clima_sence/',
   plugins: [react()],
 })
